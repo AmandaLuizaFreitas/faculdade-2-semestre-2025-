1a35cdc9f3abd9955c2492a43d1c93cf25177318
AmandaLuizaFreitas <amandalfs94@gmail.com>
2025-12-08 07:49:59 -0300

    Adiciona arquivos escopos.py e funcoes.py com funções de soma e divisão

---
+++ Aula6/escopos.py
@@ -0,0 +1,7 @@
+# escopos
+
+n1= 20
+def soma(a, b):
+    n2= 30
+    resultado= a + b + n1 + n2
+    print("Resultado da soma:", resultado)
---
+++ Aula6/funcoes.py
@@ -0,0 +1,17 @@
+# Funções em Python
+
+
+# try:
+#     resultado = n1 / n2
+#     print("Resultado da divisão:", resultado)
+# except ZeroDivisionError:
+#     print("Erro: Divisão por zero não é permitida.")
+
+def dividir(a, b):
+   
+        resultado = a / b
+        print("Resultado da divisão:", resultado) 
+        return resultado
+    
+divisao= dividir(10, 80)
+print("Divisão retornada:", divisao)
